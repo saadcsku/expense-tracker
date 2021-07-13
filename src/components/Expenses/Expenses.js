fb9e89a79aeaaaefdb314a287c13b7f593e8ab39
Saad Ur Rehman <saadcsku@gmail.com>
2021-07-13 18:18:57 +0500

    rendering list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,11 +6,8 @@ import './Expenses.css'
 const expenses=(props)=> {
     return (
         <Card className="expenses">
-            <Expenseitems title={props.items[0].title} date={props.items[0].date} amount={props.items[0].amount}></Expenseitems>
-            <Expenseitems title={props.items[1].title} date={props.items[1].date} amount={props.items[1].amount}></Expenseitems>
-            <Expenseitems title={props.items[2].title} date={props.items[2].date} amount={props.items[2].amount}></Expenseitems>
-            <Expenseitems title={props.items[3].title} date={props.items[3].date} amount={props.items[3].amount}></Expenseitems>
-        </Card>
+            {props.items.map(expense => <Expenseitems key={expense.id} title={expense.title} date={expense.date} amount={expense.amount}/>)}
+           </Card>
     )
 }
 export default expenses;
